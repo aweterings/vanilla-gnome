5124a2510d6b95d0ed0d65c8b75c90df4717b1c9
aweterings <156514352+aweterings@users.noreply.github.com>
2024-05-27 15:13:19 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,9 +14,9 @@ RELEASE="$(rpm -E %fedora)"
 
 # Install RPM Fusion repository
 
-# rpm-ostree install \
-#		https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-#		https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+rpm-ostree install \
+		https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+		https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 # rpm-ostree install rpmfusion-free-release rpmfusion-nonfree-release \
 #		--uninstall rpmfusion-free-release \
 #		--uninstall rpmfusion-nonfree-release
